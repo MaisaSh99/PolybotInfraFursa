31149e9ce397e14a0c77c9a46bed31d12fa9f830
MaisaSh99 <maisa.shaqqur@gmail.com>
2025-06-30 12:26:48 +0300

    used SSM Parameter Store in place of Secrets Manager

--- tf/modules/k8s-cluster/outputs.tf
+++ tf/modules/k8s-cluster/outputs.tf
@@ -1,10 +1,24 @@
 # tf/modules/k8s-cluster/outputs.tf
-output "control_plane_public_ip" {
-  description = "Elastic IP of the control plane EC2 instance"
-  value       = aws_eip.control_plane_eip.public_ip
+
+# Since we're using ASG, we can't have a static EIP
+# Instead, we'll output the ASG name so external scripts can query for instances
+output "control_plane_asg_name" {
+  description = "Name of the Auto Scaling Group for control plane"
+  value       = aws_autoscaling_group.control_plane_asg.name
 }
 
 output "worker_asg_name" {
   description = "Name of the Auto Scaling Group for workers"
   value       = aws_autoscaling_group.worker_asg.name
+}
+
+# You can also output the VPC and security group info for reference
+output "vpc_id" {
+  description = "ID of the VPC created for the cluster"
+  value       = aws_vpc.k8s_vpc.id
+}
+
+output "control_plane_security_group_id" {
+  description = "Security group ID for control plane"
+  value       = aws_security_group.control_plane_sg.id
 }
--- tf/outputs.tf
+++ tf/outputs.tf
@@ -1,10 +1,16 @@
 # tf/outputs.tf
-output "control_plane_public_ip" {
-  description = "Public IP of the control plane EC2 instance"
-  value       = module.k8s_cluster.control_plane_public_ip
+
+output "control_plane_asg_name" {
+  description = "Auto Scaling Group name for control plane"
+  value       = module.k8s_cluster.control_plane_asg_name
 }
 
 output "worker_asg_name" {
   description = "Auto Scaling Group name for worker nodes"
   value       = module.k8s_cluster.worker_asg_name
+}
+
+output "vpc_id" {
+  description = "VPC ID where the cluster is deployed"
+  value       = module.k8s_cluster.vpc_id
 }
