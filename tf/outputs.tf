d57f633e9ea62b58218bad1f7b6b2c3638d929d7
MaisaSh99 <maisa.shaqqur@gmail.com>
2025-06-30 12:38:18 +0300

    provision the infrastructure for a Kubernetes cluster using Terraform

--- tf/outputs.tf
+++ tf/outputs.tf
@@ -1,16 +1,10 @@
 # tf/outputs.tf
-
-output "control_plane_asg_name" {
-  description = "Auto Scaling Group name for control plane"
-  value       = module.k8s_cluster.control_plane_asg_name
+output "control_plane_public_ip" {
+  description = "Public IP of the control plane EC2 instance"
+  value       = module.k8s_cluster.control_plane_public_ip
 }
 
 output "worker_asg_name" {
   description = "Auto Scaling Group name for worker nodes"
   value       = module.k8s_cluster.worker_asg_name
-}
-
-output "vpc_id" {
-  description = "VPC ID where the cluster is deployed"
-  value       = module.k8s_cluster.vpc_id
 }
